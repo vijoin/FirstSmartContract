fb811b7c822ae4af09e2a9a46e302ff66d15bb90
Victor Inojosa <vijoin@gmail.com>
2021-11-02 11:15:02 -0300

    Initial commit "Hello World" "Hello Blockchain"
    Used:
    * truffle
    * ganache-cli

---
+++ contracts/HelloBlockchain.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloBlockchain {
+
+    function sayHi() public pure returns (string memory) {
+        return "Hello, blockchain!";
+    }
+
+}
